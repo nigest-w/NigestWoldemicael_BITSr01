aab92875be6ea938cf0a9801705aaee386a377b3
nigest-w <125654588+nigest-w@users.noreply.github.com>
2023-05-15 02:08:01 +0300

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,15 @@
+
+window.addEventListener("scroll",function () {
+    var header=document.querySelector("header")
+    header.classList.toggle("sticky",window.scrollY >0);
+    
+});
+
+
+
+
+
+
+
+
+
